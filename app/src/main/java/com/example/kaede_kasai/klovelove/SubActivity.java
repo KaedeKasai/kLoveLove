071a5bcdb3c3886d1c85b46ed6d80405bfa74f37
KaedeKasai <kimaguret@icloud.com>
2017-05-26 15:42:05 +0900

    add sub

---
+++ app/src/main/java/com/example/kaede_kasai/klovelove/SubActivity.java
@@ -0,0 +1,17 @@
+package com.example.kaede_kasai.klovelove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by kaede_kasai on 2017/05/26.
+ */
+
+public class SubActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
