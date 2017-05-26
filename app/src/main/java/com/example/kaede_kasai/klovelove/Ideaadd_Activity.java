aa1e2336eadd188a777fe10556df6436742a7f04
javaSone <marirei398@yahoo.co.jp>
2017-05-26 19:04:55 +0900

    アイデア画面追加・移動
    SAVE機能がないので追加よろしく

---
+++ app/src/main/java/com/example/kaede_kasai/klovelove/Ideaadd_Activity.java
@@ -0,0 +1,15 @@
+package com.example.kaede_kasai.klovelove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ideaadd_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ideaadd_);
+    }
+
+
+}
--- app/src/main/java/com/example/kaede_kasai/klovelove/MainActivity.java
+++ app/src/main/java/com/example/kaede_kasai/klovelove/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.kaede_kasai.klovelove;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void idea_add(View arg0) {
+
+        Intent intent = new Intent(this, Ideaadd_Activity.class);
+        startActivityForResult(intent, 0);
     }
 }
